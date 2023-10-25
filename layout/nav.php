58a9db935848218834aa28d645aafe2cbf7cc75b
Maria Jamille <maria.jamille1984@gmail.com>
2023-10-25 13:50:45 -0300

    Atualizei o modelo de recursos

--- layout/nav.php
+++ layout/nav.php
@@ -24,6 +24,7 @@
                 <!-- CONTEÚDO DO MENU LATERAL RESPONSIVO -->
                 <nav class="menulateral-responsivo">
                     <ul>
+                        <!-- ADICIONEI NOVOS ÍCONES NOS SUBTÓPICOS -->
                         <li class="menu">
                             <a href="index.php">
                                 <span class="icon "><i class="bi bi-bullseye"></i></span>
@@ -45,86 +46,22 @@
                                 <a data-valor="topico02.php">
                                     <span class="icon space"><i class="bi bi-2-circle"></i></span>
                                     <span class="txt">Usabilidade</span>
-                                        <i class="bi bi-caret-down-fill barra-sub"></i>
+                                    <i class="bi bi-caret-down-fill barra-sub"></i>
                                 </a>
                             </li>
                         </button>
                         <!-- subtopicos aqui  -->
                         <div class="dropdown-container">
                             <a href="topico02.php">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
+                                <i class="bi bi-journals circulo"></i>Início</a>
                             <a href="#item2.1">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>2.1 - NBR ISO 9241-11</a>
+                                <i class="bi bi-journals circulo"></i>2.1 - NBR ISO 9241-11</a>
                             <a href="#item2.2">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>2.2 -ISO/IEC 25000 (SQuaRE)</a>
+                                <i class="bi bi-journals circulo"></i>2.2 -ISO/IEC 25000 (SQuaRE)</a>
                             <a href="#item2.3">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>2.3 - Heurísticas de Nielsen</a>
+                                <i class="bi bi-journals circulo"></i>2.3 - Heurísticas de Nielsen</a>
                             <a href="#item2.4">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>2.4 - O que não é usual?</a>
-                        </div>
-
-                        <!-- topico com Subtópico -->
-                        <button class="dropdown-btn">
-                            <li class="menu">
-                                <a data-valor="topico03.php">
-                                    <span class="icon space"><i class="bi bi-3-circle"></i></span>
-                                    <span class="txt">Heurísticas de Nielsen</span>
-                                        <i class="bi bi-caret-down-fill barra-sub"></i>
-                                </a>
-                            </li>
-                        </button>
-                        <!-- subtopicos aqui  -->
-                        <div class="dropdown-container">
-                            <a href="topico03.php">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
-                            <a href="#item3.1">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.1 - Visibilidade de qual estado
-                                estamos no sistema</a>
-                            <a href="#item3.2">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.2 - Correspondência entre o sistema
-                                e o mundo real</a>
-                            <a href="#item3.3">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.3 - Liberdade de controle fácil pro
-                                usuário</a>
-                            <a href="#item3.4">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.4 - Consistência e Padrões</a>
-                            <a href="#item3.5">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.5 - Prevenção de Erros</a>
-                            <a href="#item3.6">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.6 - Reconhecimento em vez de
-                                memorização</a>
-                            <a href="#item3.7">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.7 - Flexibilidade e Eficiência de
-                                Uso</a>
-                            <a href="#item3.8">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.8 - Estética e design
-                                minimalista</a>
-                            <a href="#item3.9">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.9 - Ajude os Usuários a
-                                Reconhecerem, Diagnosticarem e
-                                Recuperarem-se de Erros</a>
-                            <a href="#item3.10">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>3.10 - Ajuda e Documentação</a>
-                        </div>
-
-                        <!-- topico com Subtópico -->
-                        <button class="dropdown-btn">
-                            <li class="menu">
-                                <a data-valor="topico04.php">
-                                    <span class="icon space"><i class="bi bi-4-circle"></i></span>
-                                    <span class="txt">Avaliação de Usabilidade</span>
-                                        <i class="bi bi-caret-down-fill barra-sub"></i>
-                                </a>
-                            </li>
-                        </button>
-                        <!-- subtopicos aqui  -->
-                        <div class="dropdown-container">
-                            <a href="topico04.php">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>Início</a>
-                            <a href="#item4.1">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>4.1 - Por que avaliar?</a>
-                            <a href="#item4.2">
-                                <i class="fa-regular fa-pen-to-square circulo"></i>4.2 - Como Avaliar</a>
+                                <i class="bi bi-journals circulo"></i>2.4 - O que não é usual?</a>
                         </div>
 
                         <li class="menu">
